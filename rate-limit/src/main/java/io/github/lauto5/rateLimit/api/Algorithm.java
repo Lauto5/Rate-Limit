6e13283e468b3ce83af57377a4d2047dbaac98af
lauto5 <laugtaro6@gmail.com>
2026-08-28 23:27:46 -0300

    Add : agregando el tokenBucket al factory Algorithm.

--- rate-limit/src/main/java/io/github/lauto5/rateLimit/api/Algorithm.java
+++ rate-limit/src/main/java/io/github/lauto5/rateLimit/api/Algorithm.java
@@ -2,11 +2,17 @@ package io.github.lauto5.rateLimit.api;
 
 import io.github.lauto5.rateLimit.domain.algorithm.FixedWindowAlgorithm;
 import io.github.lauto5.rateLimit.domain.algorithm.FixedWindowAlgorithmImpl;
+import io.github.lauto5.rateLimit.domain.algorithm.TokenBucketAlgorithm;
+import io.github.lauto5.rateLimit.domain.algorithm.TokenBucketAlgorithmImpl;
 
 public class Algorithm {
 
 	public static FixedWindowAlgorithm fixedWindow() {
 		return new FixedWindowAlgorithmImpl();
+	}
+	
+	public static TokenBucketAlgorithm tokenBucket() {
+		return new TokenBucketAlgorithmImpl();
 	}
 	
 }
